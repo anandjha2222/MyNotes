e424c4378b6f2f574fb850279c55a1f1e82cde1b
Anand Kumar <anandjha2222@gmail.com>
2018-11-01 09:04:59 +0530

    Added a test file to understand git stash command

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test{
+	
+	public static void main(String[] args){
+		
+	}
+	
+	/*Add method*/
+	private int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	/*Subtract method*/
+	private int sub(int a, int b){
+		return a-b;
+	}
+	
+	/*Multiply method*/
+	private int mul(int a, int b){
+		return a*b;
+	}
+}
